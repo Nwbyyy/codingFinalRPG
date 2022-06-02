5a3048048855841b78b0d049633b4ece2fbfed00
Nwbyyy <103236125+Nwbyyy@users.noreply.github.com>
2022-06-02 09:50:31 -0700

    Added boss attack sequence

--- codingFinalRPG/src/codingFinalRPG/Combat.java
+++ codingFinalRPG/src/codingFinalRPG/Combat.java
@@ -32,4 +32,54 @@ public class Combat {
 
 	}
 	
+	//Condensed to do the attacking sequence (when you chose to attack)
+		public static boolean BossAttackSeq(Character player, Enemy boss) {
+
+			if(((int) (Math.random()*3)) == 0) {
+
+				if(((int) (Math.random()*2)) == 0) {
+					
+					boss.takeDamage(player.getInv().get(0).getDamage());
+	                
+					RPGMethods.DisplayYourAttack(player, boss);
+									
+					RPGMethods.DisplayEnemyMissed(player);
+					
+					RPGMethods.DisplayBossWeak();
+					
+					return true;
+				}
+				
+				else {
+					
+					boss.takeDamage(player.getInv().get(0).getDamage());
+	                
+					RPGMethods.DisplayYourAttack(player, boss);
+									
+					RPGMethods.DisplayEnemyMissed(player);
+					
+					return false;
+					
+				}
+				
+
+			}
+
+			else {
+
+				boss.takeDamage(player.getInv().get(0).getDamage());
+			                
+				RPGMethods.DisplayYourAttack(player, boss);
+								
+				boss.enemyAttack(player);
+								
+				RPGMethods.DisplayEnemyAttack(player, boss);
+				
+				return false;
+
+			}
+			
+
+		}
+	
 }
